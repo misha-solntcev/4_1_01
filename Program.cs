4ad34864966d1ec00348bf6d9c92d3e29cffc49c
misha-solntcev <misha.solntcev@gmail.com>
2022-09-23 14:33:43 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Вариант 1
+В заданной матрице A(N, M) найдите индексы первого элемента, превосходящего
+среднее арифметическое всех элементов. */
+
+namespace _4_1_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,]
+            {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 },
+            };
+
+            int sum = 0;
+            foreach (int i in array)
+                sum += i;
+            double average = sum / array.Length;
+
+            for (int i = 0; i < array.GetLength(0); i++)
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (array[i, j] > average)
+                    {
+                        Console.WriteLine($"Индекс первого " +
+                            $"элемента, превосходящего среднее " +
+                            $"арифметическое всех элементов  - {array[i, j]}");
+                    }
+                    break;
+                }            
+            Console.ReadKey();
+        }
+    }
+}
